f0488f4915d893090d6d6de2ba9dc4b1161c5679
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-08-04 12:03:32 +0200

    add dependency injection extension method

---
+++ src/Cayd.AspNetCore.Mediator/DependencyInjection/AddMediator.cs
@@ -0,0 +1,44 @@
+﻿using Cayd.AspNetCore.Mediator.Abstraction;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Cayd.AspNetCore.Mediator.DependencyInjection
+{
+    public static class MediatorDependencyInjection
+    {
+        public static void AddMediator(this IServiceCollection services, Action<MediatorConfig> configure)
+        {
+            var config = new MediatorConfig();
+            configure(config);
+
+            foreach (var assembly in config.GetAssemblies())
+            {
+                AddAllHandlersInAssembly(services, assembly);
+            }
+
+            services.AddScoped<IMediator, Mediator>();
+        }
+
+        private static void AddAllHandlersInAssembly(IServiceCollection services, Assembly assembly)
+        {
+            var allHandlers = assembly.GetTypes()
+                .Where(t => t.IsClass && !t.IsAbstract && 
+                    t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IAsyncRequestHandler<,>)))
+                .Select(t => new
+                {
+                    InterfaceType = t.GetInterfaces()
+                        .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IAsyncRequestHandler<,>))
+                        .First(),
+                    ImplementationType = t
+                })
+                .ToList();
+
+            foreach (var handler in allHandlers)
+            {
+                services.AddScoped(handler.InterfaceType, handler.ImplementationType);
+            }
+        }
+    }
+}
---
+++ src/Cayd.AspNetCore.Mediator/DependencyInjection/MediatorConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Cayd.AspNetCore.Mediator.DependencyInjection
+{
+    public class MediatorConfig
+    {
+        private List<Assembly> _assemblies = new List<Assembly>();
+
+        public MediatorConfig RegisterHandlersFromAssembly(Assembly assembly)
+        {
+            _assemblies.Add(assembly);
+            return this;
+        }
+
+        public MediatorConfig RegisterHandlersFromAssemblies(params Assembly[] assemblies)
+        {
+            _assemblies.AddRange(assemblies);
+            return this;
+        }
+
+        public List<Assembly> GetAssemblies() => _assemblies;
+    }
+}
